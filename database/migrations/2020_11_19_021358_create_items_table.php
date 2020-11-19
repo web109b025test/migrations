40af4a2a2bf00edf37b3a48e1486fea83e1a4015
Weian Kuo <duetobe@yahoo.com.tw>
2020-11-19 10:42:27 +0800

    no message

--- database/migrations/2020_11_19_021358_create_items_table.php
+++ database/migrations/2020_11_19_021358_create_items_table.php
@@ -14,22 +14,13 @@ class CreateItemsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            // $table->id();
-            // $table->timestamps();
-            // $table->bigIncrements(); //(欄位名稱) 自動流水號
-            // $table->boolean(); //(欄位名稱) 布林值
-            // $table->integer(); //(欄位名稱) 整數
-            // $table->string(); //(欄位名稱,長度) 長度為xx的字串
-            // $table->text(); //(欄位名稱) 長字串
-            // $table->date(); //(欄位名稱) 日期
-            // $table->timestamp(); //(欄位名稱) 時間戳記
-            // $table->datetime(); //(欄位名稱) 日期加時間
-            $table->BidInteger('id');
-            $table->string(50)('title');
-            $table->Integer('price')->default(0)->unsigned;
-            $table->Text('desc')->nullable;
-            $table->Bool('enabled')->default(true);
-            $table->Timestamp('sell-at')->nullable;
+
+            $table->BigInteger('id');
+            $table->string('title', 50);
+            $table->Integer('price')->default(0)->unsigned();
+            $table->Text('desc')->nullable();
+            $table->boolean('enabled')->default(true);
+            $table->Timestamp('sell-at')->nullable();
             $table->BigInteger('cgy-id');
         });
     }
